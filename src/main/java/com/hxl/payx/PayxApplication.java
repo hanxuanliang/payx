678b44e9bbde5aed03063041b1403b138c0e49d0
hanhotfox <hxuanliang@163.com>
2020-01-15 08:18:29 +0800

    :tada: project init

---
+++ src/main/java/com/hxl/payx/PayxApplication.java
@@ -0,0 +1,13 @@
+package com.hxl.payx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hxl/payx/PayxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hxl.payx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
